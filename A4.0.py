ebc9ded3fafd4b2ec22ffb8ba65bee8c49ece6d4
studentristan <54915507+studentristan@users.noreply.github.com>
2022-02-01 20:47:05 -0500

    Add files via upload

---
+++ A4.0.py
@@ -0,0 +1,12 @@
+
+def newmax(a, b, c):
+    if (a > b) and (a > c) :
+        print(a)
+    elif (b > a) and (b > c) :
+        print (b)
+    else :
+        print (c)
+
+newmax(3, 2, 1)
+newmax(1, 2, 3)
+newmax(2, 3, 1)
